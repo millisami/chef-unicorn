dc3d046fc0a83978fd414900d57024b1ab48f998
Scott M. Likens <scott@likens.us>
2012-02-21 16:12:22 -0800

    I did it for the lulz!

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-maintainer       "Opscode, Inc"
-maintainer_email "cookbooks@opscode.com"
+maintainer       "Scott M. Likens"
+maintainer_email "scott@likens.us"
 license          "Apache 2.0"
 description      "Installs/Configures unicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 recipe "unicorn", "Installs unicorn rubygem"
